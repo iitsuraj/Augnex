28c1c830e50af65df79c667f53efeb16ef68ee0e
unknown <34763906+iitsuraj@users.noreply.github.com>
2020-03-16 00:00:24 +0530

    Models Update Or Vishant Added To repo

--- ar.js
+++ ar.js
@@ -11,28 +11,33 @@ var data = [
         name: "lcdirection",
         center: { lat: 25.260654, long: 82.986879 },
         model: "./models/lc.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 180 0"
+      },
+      {
+        name: "Welcomelc",
+        center: { lat: 25.260654, long: 82.986879 },
+        model: "./models/Welcome.gltf",
+        scale: "30 30 30",
+        render: false,
+        rotation: "0 0 0",
+        animation: true
       }
     ],
     points: [
-      new L.LatLng(25.26001, 82.984468),
-      new L.LatLng(25.26009, 82.984446),
-      new L.LatLng(25.260609, 82.986391),
-      new L.LatLng(25.26106, 82.986753),
-      new L.LatLng(25.262606, 82.986494),
-      new L.LatLng(25.262613, 82.986639),
-      new L.LatLng(25.261356, 82.986819),
-      new L.LatLng(25.260883, 82.987518),
-      new L.LatLng(25.26126, 82.989204),
-      new L.LatLng(25.26113, 82.989282),
-      new L.LatLng(25.260681, 82.987215),
-      new L.LatLng(25.260251, 82.987063),
-      new L.LatLng(25.25902, 82.987606),
-      new L.LatLng(25.258964, 82.987503),
-      new L.LatLng(25.260396, 82.986903),
-      new L.LatLng(25.260505, 82.986483)
+      new L.LatLng(25.263607, 82.986999),
+      new L.LatLng(25.261482, 82.987321),
+      new L.LatLng(25.261909, 82.990014),
+      new L.LatLng(25.261172, 82.990239),
+      new L.LatLng(25.260396, 82.987482),
+      new L.LatLng(25.258591, 82.988297),
+      new L.LatLng(25.258232, 82.987503),
+      new L.LatLng(25.260212, 82.986613),
+      new L.LatLng(25.259513, 82.983823),
+      new L.LatLng(25.26028, 82.983491),
+      new L.LatLng(25.260978, 82.986409),
+      new L.LatLng(25.263346, 82.986023)
     ]
   },
   {
@@ -44,26 +49,33 @@ var data = [
         name: "ccd",
         center: { lat: 25.258502, long: 82.987814 },
         model: "./models/ccd.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 270 0"
+      },
+      {
+        name: "Adv welcome",
+        center: { lat: 25.259153, long: 82.989267 },
+        model: "./models/Welcome.gltf",
+        scale: "30 30 30",
+        render: false,
+        rotation: "0 0 0",
+        animation: true
       }
     ],
     points: [
-      new L.LatLng(25.258341, 82.987868),
-      new L.LatLng(25.258363, 82.98794),
-      new L.LatLng(25.258513, 82.987921),
-      new L.LatLng(25.259732, 82.990583),
-      new L.LatLng(25.2598, 82.990553),
-      new L.LatLng(25.258578, 82.987883),
-      new L.LatLng(25.258609, 82.987796),
-      new L.LatLng(25.260582, 82.98693),
-      new L.LatLng(25.260568, 82.986871),
-      new L.LatLng(25.258589, 82.987721),
-      new L.LatLng(25.258499, 82.987715),
-      new L.LatLng(25.257222, 82.985022),
-      new L.LatLng(25.257171, 82.985048),
-      new L.LatLng(25.258428, 82.987746)
+      new L.LatLng(25.260793, 82.986527),
+      new L.LatLng(25.258756, 82.987214),
+      new L.LatLng(25.257621, 82.984596),
+      new L.LatLng(25.256767, 82.985025),
+      new L.LatLng(25.257941, 82.987675),
+      new L.LatLng(25.257553, 82.987932),
+      new L.LatLng(25.257999, 82.988759),
+      new L.LatLng(25.258348, 82.988523),
+      new L.LatLng(25.259474, 82.991033),
+      new L.LatLng(25.260347, 82.990615),
+      new L.LatLng(25.25927, 82.98805),
+      new L.LatLng(25.260958, 82.98731)
     ]
   },
   {
@@ -74,27 +86,25 @@ var data = [
       {
         name: "gtac",
         center: { lat: 25.259897, long: 82.983791 },
-        model: "./models/direction2atgamcha.gltf",
-        scale: "60 60 60",
+        model: "./models/gtac.gltf",
+        scale: "30 30 30",
         render: false,
         rotation: " 0 90 0"
       }
     ],
     points: [
-      new L.LatLng(25.262657, 82.98295),
-      new L.LatLng(25.260484, 82.983358),
-      new L.LatLng(25.259989, 82.983358),
-      new L.LatLng(25.259581, 82.983465),
-      new L.LatLng(25.259465, 82.983765),
-      new L.LatLng(25.25733, 82.98471),
-      new L.LatLng(25.257495, 82.985064),
-      new L.LatLng(25.25962, 82.984184),
-      new L.LatLng(25.259824, 82.984355),
-      new L.LatLng(25.260396, 82.986694),
-      new L.LatLng(25.260784, 82.986512),
-      new L.LatLng(25.26028, 82.984205),
-      new L.LatLng(25.260536, 82.983787),
-      new L.LatLng(25.262822, 82.983415)
+      new L.LatLng(25.263315, 82.982914),
+      new L.LatLng(25.260183, 82.983314),
+      new L.LatLng(25.260115, 82.982595),
+      new L.LatLng(25.259271, 82.982767),
+      new L.LatLng(25.259505, 82.983508),
+      new L.LatLng(25.256922, 82.984684),
+      new L.LatLng(25.257179, 82.98533),
+      new L.LatLng(25.259704, 82.984361),
+      new L.LatLng(25.260405, 82.987127),
+      new L.LatLng(25.261071, 82.986984),
+      new L.LatLng(25.260639, 82.984258),
+      new L.LatLng(25.263326, 82.98356)
     ]
   },
   {
@@ -106,28 +116,22 @@ var data = [
         name: "karma",
         center: { lat: 25.257157, long: 82.984955 },
         model: "./models/karma.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 0 0"
       }
     ],
     points: [
-      new L.LatLng(25.257011, 82.984177),
-      new L.LatLng(25.257263, 82.984628),
-      new L.LatLng(25.257593, 82.984585),
-      new L.LatLng(25.259611, 82.983662),
-      new L.LatLng(25.259611, 82.983662),
-      new L.LatLng(25.257739, 82.984949),
-      new L.LatLng(25.257525, 82.985325),
-      new L.LatLng(25.258583, 82.98746),
-      new L.LatLng(25.258224, 82.987664),
-      new L.LatLng(25.257234, 82.985625),
-      new L.LatLng(25.256875, 82.985336),
-      new L.LatLng(25.256002, 82.985851),
-      new L.LatLng(25.255895, 82.985486),
-      new L.LatLng(25.256739, 82.984992),
-      new L.LatLng(25.256865, 82.984649),
-      new L.LatLng(25.256652, 82.984338)
+      new L.LatLng(25.260183, 82.984154),
+      new L.LatLng(25.257951, 82.985214),
+      new L.LatLng(25.258932, 82.987747),
+      new L.LatLng(25.258184, 82.988134),
+      new L.LatLng(25.256922, 82.985615),
+      new L.LatLng(25.256738, 82.984853),
+      new L.LatLng(25.256408, 82.984135),
+      new L.LatLng(25.257214, 82.983748),
+      new L.LatLng(25.257427, 82.984478),
+      new L.LatLng(25.259949, 82.983437)
     ]
   },
   {
@@ -139,28 +143,33 @@ var data = [
         name: "lake",
         center: { lat: 25.263332, long: 82.989657 },
         model: "./models/iit.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 180 0"
+      },
+      {
+        name: "lake welcome",
+        center: { lat: 25.262, long: 82.989869 },
+        model: "./models/Welcome.gltf",
+        scale: "30 30 30",
+        render: false,
+        rotation: "0 0 0",
+        animation: true
       }
     ],
     points: [
-      new L.LatLng(25.263086, 82.98665),
-      new L.LatLng(25.263175, 82.98926),
-      new L.LatLng(25.262862, 82.989571),
-      new L.LatLng(25.259885, 82.990467),
-      new L.LatLng(25.260026, 82.990825),
-      new L.LatLng(25.263055, 82.989831),
-      new L.LatLng(25.263217, 82.990016),
-      new L.LatLng(25.263374, 82.992638),
-      new L.LatLng(25.263666, 82.992603),
-      new L.LatLng(25.263536, 82.990137),
-      new L.LatLng(25.263739, 82.989814),
-      new L.LatLng(25.265466, 82.990027),
-      new L.LatLng(25.265446, 82.989693),
-      new L.LatLng(25.263908, 82.989471),
-      new L.LatLng(25.263516, 82.98916),
-      new L.LatLng(25.263375, 82.986681)
+      new L.LatLng(25.266072, 82.98951),
+      new L.LatLng(25.263908, 82.989048),
+      new L.LatLng(25.263899, 82.986323),
+      new L.LatLng(25.262676, 82.986302),
+      new L.LatLng(25.26287, 82.989134),
+      new L.LatLng(25.259435, 82.990422),
+      new L.LatLng(25.259814, 82.991183),
+      new L.LatLng(25.263006, 82.990153),
+      new L.LatLng(25.26322, 82.9929),
+      new L.LatLng(25.263941, 82.992892),
+      new L.LatLng(25.263887, 82.990118),
+      new L.LatLng(25.266568, 82.990666)
     ]
   },
   {
@@ -172,28 +181,24 @@ var data = [
         name: "dg",
         center: { lat: 25.263211, long: 82.986463 },
         model: "./models/dg.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 180 0"
       }
     ],
     points: [
-      new L.LatLng(25.263232, 82.984621),
-      new L.LatLng(25.262976, 82.984656),
-      new L.LatLng(25.263075, 82.986139),
-      new L.LatLng(25.262827, 82.986396),
-      new L.LatLng(25.26125, 82.986605),
-      new L.LatLng(25.261272, 82.986975),
-      new L.LatLng(25.262954, 82.986621),
-      new L.LatLng(25.263047, 82.9869),
-      new L.LatLng(25.263183, 82.989401),
-      new L.LatLng(25.263502, 82.989342),
-      new L.LatLng(25.263399, 82.986801),
-      new L.LatLng(25.263632, 82.986592),
-      new L.LatLng(25.266432, 82.986998),
-      new L.LatLng(25.266519, 82.986616),
-      new L.LatLng(25.263513, 82.986275),
-      new L.LatLng(25.263361, 82.985976)
+      new L.LatLng(25.266858, 82.986313),
+      new L.LatLng(25.263811, 82.985991),
+      new L.LatLng(25.263588, 82.983094),
+      new L.LatLng(25.262472, 82.983094),
+      new L.LatLng(25.262812, 82.986023),
+      new L.LatLng(25.260474, 82.986484),
+      new L.LatLng(25.2606, 82.98731),
+      new L.LatLng(25.262841, 82.986946),
+      new L.LatLng(25.262948, 82.989799),
+      new L.LatLng(25.263734, 82.98981),
+      new L.LatLng(25.263724, 82.986988),
+      new L.LatLng(25.267227, 82.987514)
     ]
   },
   {
@@ -205,28 +210,24 @@ var data = [
         name: "civildep",
         center: { lat: 25.263479, long: 82.992751 },
         model: "./models/civil.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 0 0"
       }
     ],
     points: [
-      new L.LatLng(25.263188, 82.9899),
-      new L.LatLng(25.263268, 82.992122),
-      new L.LatLng(25.262823, 82.992574),
-      new L.LatLng(25.261169, 82.993105),
-      new L.LatLng(25.261312, 82.993537),
-      new L.LatLng(25.26301, 82.992977),
-      new L.LatLng(25.26333, 82.993184),
-      new L.LatLng(25.263517, 82.995602),
-      new L.LatLng(25.263872, 82.995582),
-      new L.LatLng(25.26373, 82.993537),
-      new L.LatLng(25.264093, 82.992867),
-      new L.LatLng(25.264319, 82.992902),
-      new L.LatLng(25.264345, 82.992612),
-      new L.LatLng(25.263851, 82.992569),
-      new L.LatLng(25.263667, 82.992331),
-      new L.LatLng(25.263581, 82.989863)
+      new L.LatLng(25.265291, 82.992477),
+      new L.LatLng(25.263837, 82.992431),
+      new L.LatLng(25.263777, 82.989561),
+      new L.LatLng(25.262987, 82.989548),
+      new L.LatLng(25.263167, 82.992419),
+      new L.LatLng(25.26078, 82.993023),
+      new L.LatLng(25.261169, 82.993832),
+      new L.LatLng(25.263208, 82.993138),
+      new L.LatLng(25.263434, 82.996343),
+      new L.LatLng(25.26409, 82.996291),
+      new L.LatLng(25.263813, 82.992998),
+      new L.LatLng(25.265139, 82.993148)
     ]
   },
   {
@@ -238,28 +239,24 @@ var data = [
         name: "sb",
         center: { lat: 25.261059, long: 82.993406 },
         model: "./models/sb.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 0 0"
       }
     ],
     points: [
-      new L.LatLng(25.259711, 82.990903),
-      new L.LatLng(25.26076, 82.993125),
-      new L.LatLng(25.260618, 82.993508),
-      new L.LatLng(25.259382, 82.994393),
-      new L.LatLng(25.259676, 82.994747),
-      new L.LatLng(25.260654, 82.993911),
-      new L.LatLng(25.261089, 82.993911),
-      new L.LatLng(25.262067, 82.996103),
-      new L.LatLng(25.262396, 82.995907),
-      new L.LatLng(25.261561, 82.99399),
-      new L.LatLng(25.261747, 82.993439),
-      new L.LatLng(25.263357, 82.992967),
-      new L.LatLng(25.263357, 82.992495),
-      new L.LatLng(25.261596, 82.992869),
-      new L.LatLng(25.261009, 82.992741),
-      new L.LatLng(25.260103, 82.990804)
+      new L.LatLng(25.263646, 82.992324),
+      new L.LatLng(25.261377, 82.992601),
+      new L.LatLng(25.260338, 82.99033),
+      new L.LatLng(25.259383, 82.990884),
+      new L.LatLng(25.260494, 82.993221),
+      new L.LatLng(25.258654, 82.995017),
+      new L.LatLng(25.259204, 82.995638),
+      new L.LatLng(25.26088, 82.9942),
+      new L.LatLng(25.262225, 82.996824),
+      new L.LatLng(25.262917, 82.99655),
+      new L.LatLng(25.261718, 82.993639),
+      new L.LatLng(25.263684, 82.993174)
     ]
   },
   {
@@ -271,28 +268,24 @@ var data = [
         name: "lt1",
         center: { lat: 25.259807, long: 82.990731 },
         model: "./models/lt1.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 0 0"
       }
     ],
     points: [
-      new L.LatLng(25.258727, 82.987886),
-      new L.LatLng(25.259826, 82.990265),
-      new L.LatLng(25.260306, 82.990265),
-      new L.LatLng(25.26317, 82.989418),
-      new L.LatLng(25.263219, 82.989979),
-      new L.LatLng(25.260541, 82.990612),
-      new L.LatLng(25.260327, 82.991213),
-      new L.LatLng(25.261162, 82.993165),
-      new L.LatLng(25.260861, 82.993337),
-      new L.LatLng(25.259881, 82.991331),
-      new L.LatLng(25.259473, 82.991127),
-      new L.LatLng(25.258493, 82.991846),
-      new L.LatLng(25.258289, 82.991561),
-      new L.LatLng(25.259338, 82.990785),
-      new L.LatLng(25.259365, 82.990264),
-      new L.LatLng(25.258458, 82.988042)
+      new L.LatLng(25.263456, 82.989154),
+      new L.LatLng(25.260173, 82.990011),
+      new L.LatLng(25.259042, 82.987273),
+      new L.LatLng(25.257998, 82.987821),
+      new L.LatLng(25.25915, 82.990511),
+      new L.LatLng(25.25789, 82.991654),
+      new L.LatLng(25.258375, 82.992321),
+      new L.LatLng(25.259656, 82.991297),
+      new L.LatLng(25.260811, 82.993827),
+      new L.LatLng(25.261582, 82.993316),
+      new L.LatLng(25.2604, 82.990882),
+      new L.LatLng(25.263483, 82.989963)
     ]
   },
   {
@@ -304,26 +297,24 @@ var data = [
         name: "sryia",
         center: { lat: 25.263026, long: 82.983826 },
         model: "./models/sryia.gltf",
-        scale: "60 60 60",
+        scale: "30 30 30",
         render: false,
         rotation: "0 270 0"
       }
     ],
     points: [
-      new L.LatLng(25.26582, 82.983605),
-      new L.LatLng(25.265859, 82.983196),
-      new L.LatLng(25.263488, 82.983023),
-      new L.LatLng(25.263182, 82.982828),
-      new L.LatLng(25.26281, 82.982776),
-      new L.LatLng(25.262587, 82.983013),
-      new L.LatLng(25.260153, 82.983434),
-      new L.LatLng(25.260148, 82.98386),
-      new L.LatLng(25.262669, 82.983402),
-      new L.LatLng(25.262889, 82.983575),
-      new L.LatLng(25.263028, 82.985859),
-      new L.LatLng(25.263307, 82.985784),
-      new L.LatLng(25.263168, 82.983561),
-      new L.LatLng(25.263354, 82.983435)
+      new L.LatLng(25.266049, 82.983818),
+      new L.LatLng(25.263747, 82.983676),
+      new L.LatLng(25.26363, 82.986661),
+      new L.LatLng(25.262847, 82.986713),
+      new L.LatLng(25.262649, 82.983663),
+      new L.LatLng(25.259902, 82.98418),
+      new L.LatLng(25.259692, 82.983547),
+      new L.LatLng(25.262847, 82.982849),
+      new L.LatLng(25.262554, 82.982163),
+      new L.LatLng(25.263631, 82.982034),
+      new L.LatLng(25.263397, 82.982849),
+      new L.LatLng(25.266236, 82.983108)
     ]
   }
 ];
@@ -384,10 +375,22 @@ window.onload = () => {
                 );
                 icon.setAttribute("name", object.name);
                 icon.setAttribute("gltf-model", object.model);
-                 icon.setAttribute("scale", "30 30 30");
+                icon.setAttribute("scale", object.scale);
                 icon.setAttribute("rotation", object.rotation);
                 icon.setAttribute("id", object.name);
+                if (object.animation) {
+                  icon.setAttribute("animation-mixer", "");
+                  icon.setAttribute(
+                    "animation",
+                    "property: rotation; to: 0 360 0;loop:true; dur: 8000"
+                  );
+                }
+
                 // icon.setAttribute("position", "0 200 0");
+                // icon.setAttribute(
+                //   ("animation",
+                //   "property: rotation; dur: 10000; to: 0 360 0; loop: true")
+                // );
                 scene.appendChild(icon);
                 object.render = !object.render;
               }
